e6047882538f41532099baef812892619b2cce78
kacper <vkacperpl@gmail.com>
2026-09-23 13:00:42 +0200

    - Added SecurityConfig - Added Json properties to Citizen

--- src/main/java/dev/kacperm/smartcity/dao/Citizen.java
+++ src/main/java/dev/kacperm/smartcity/dao/Citizen.java
@@ -1,5 +1,6 @@
 package dev.kacperm.smartcity.dao;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,11 +16,17 @@ public class Citizen {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountId;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
+    @JsonProperty("middle_names")
     private String[] middleNames;
+    @JsonProperty("email")
     private String email;
+    @JsonProperty("password")
     private String password;
+    @JsonProperty("ssn")
     private int ssn;
 
     public Citizen() {
